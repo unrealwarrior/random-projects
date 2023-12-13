e162a6ba26c6949982e28ea2374546e3a08647fe
Mike Pierce <unrealwarrior97@gmail.com>
2023-12-13 14:37:09 +0100

    basic calculator functions are completed

---
+++ pycalc/cli.py
@@ -0,0 +1,61 @@
+import math
+from decimal import Decimal
+class BasicCalc():
+    VALID_OPERATORS = ["+", "-", "*", "/"]
+    VALID_OPERATIONS = ["sqrt", "reciproc"]
+    def __init__(self) -> None:
+        pass
+    
+    # basic, but it gets the job done
+    def reciproc(self, number):
+        return (1 / number)
+    
+    def get_user_input(self, value_string):
+        user_input = input("Enter the %s value: " % value_string)
+        while not user_input.isnumeric() and (float(user_input).is_integer()):
+            print("Input is not a number. Try again.")
+            user_input = input("Enter the %s value: " % value_string)
+        return user_input
+    
+
+    def get_user_operator(self):
+        opr = input("Enter the operator: ")
+        while opr not in self.VALID_OPERATIONS and opr not in self.VALID_OPERATORS:
+            print(f"Wrong operator, try again with the following: {self.VALID_OPERATORS}")
+            opr = input("Enter the operator: ")
+        return opr
+    
+    def evaluate_results(self, first_input, operator, second_input=None):
+        if first_input == None:
+            raise ValueError("You need at least to enter the first input.")
+        if operator in self.VALID_OPERATORS:
+            if second_input == None:
+                raise ValueError("You need to enter the second input.")
+            res = eval(f"{first_input} {operator} {second_input}")
+            print(f"The result of {first_input} {operator} {second_input} is: {res}")
+            return True 
+        elif operator == "sqrt":
+            res = math.floor(math.sqrt(int(first_input)))
+            print(f"The square root of {first_input} is: {res}")
+            return 0
+        elif operator == "reciproc":
+            res = self.reciproc(int(first_input))
+            print(f"Reciprocal of {first_input} is {res}")
+            return 0
+        
+        
+        
+    def main(self):
+        first_input = self.get_user_input("First")
+        opr = self.get_user_operator()
+        if opr in self.VALID_OPERATIONS:
+            return self.evaluate_results(first_input, operator=opr)
+        else:
+            second_input = self.get_user_input("Second")
+            return self.evaluate_results(first_input, operator=opr, second_input=second_input)
+
+
+
+
+x = BasicCalc()
+x.main()
