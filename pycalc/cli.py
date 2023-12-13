e0345c954a37b0a43c4e6fad3b5cee8f3d444d5b
Mike Pierce <unrealwarrior97@gmail.com>
2023-12-13 21:57:56 +0100

    more stuff added

--- pycalc/cli.py
+++ pycalc/cli.py
@@ -1,11 +1,36 @@
 import math
-from decimal import Decimal
+import sys
 class BasicCalc():
     VALID_OPERATORS = ["+", "-", "*", "/"]
     VALID_OPERATIONS = ["sqrt", "reciproc"]
+
+    OPERATIONS = {
+        "+" : "Add Number",
+        "/" : "Divide Number",
+        "*" : "Multiply Number",
+        "-" : "Subtract Number",
+        "sqrt": "Get the square root of a number",
+        "reciproc" : "reciproc = Get the reciprocal of a number",
+        "e": "\n Exit the program."
+
+    }
     def __init__(self) -> None:
         pass
     
+    # display input messages
+    def display_available_methods(self):
+        for key, value in self.OPERATIONS.items():
+            print(f"{key} = {value}")
+        opr = input("\n > Choose from available methods: ")
+        if opr in ["e", "E"]:
+            sys.exit()
+        while opr not in self.OPERATIONS.keys():
+            opr = input("\n > Choose from available methods: ")
+            print("Invalid operator.")
+        print(f"User input : {opr}")
+        return opr
+
+
     # basic, but it gets the job done
     def reciproc(self, number):
         return (1 / number)
@@ -32,27 +57,40 @@ class BasicCalc():
             if second_input == None:
                 raise ValueError("You need to enter the second input.")
             res = eval(f"{first_input} {operator} {second_input}")
-            print(f"The result of {first_input} {operator} {second_input} is: {res}")
-            return True 
+            # print(f"The result of {first_input} {operator} {second_input} is: {res}")
+            return res 
         elif operator == "sqrt":
             res = math.floor(math.sqrt(int(first_input)))
             print(f"The square root of {first_input} is: {res}")
-            return 0
         elif operator == "reciproc":
             res = self.reciproc(int(first_input))
             print(f"Reciprocal of {first_input} is {res}")
-            return 0
         
         
         
     def main(self):
-        first_input = self.get_user_input("First")
-        opr = self.get_user_operator()
-        if opr in self.VALID_OPERATIONS:
-            return self.evaluate_results(first_input, operator=opr)
-        else:
-            second_input = self.get_user_input("Second")
-            return self.evaluate_results(first_input, operator=opr, second_input=second_input)
+        while True:
+            first_input = self.get_user_input("First")
+            # opr = self.get_user_operator()
+            opr = self.display_available_methods()
+            if opr in self.VALID_OPERATIONS:
+                self.evaluate_results(first_input, operator=opr)
+            else:
+                second_input = self.get_user_input("Second")
+                res = self.evaluate_results(first_input, operator=opr, second_input=second_input)
+                print(f"{first_input} {opr} {second_input} = {res}")
+
+                while True:
+                    opr = self.display_available_methods()
+                    if opr in self.VALID_OPERATIONS:
+                        self.evaluate_results(res, operator=opr)
+                    else:
+                        x = input("Enter next input: ")
+                        res = self.evaluate_results(first_input=res, operator=opr, second_input=x)
+                        print(f"{res} {opr} {x} = {res}")
+
+
+
 
 
 
