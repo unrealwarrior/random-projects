7fb559c9ec89288f9e523b62476a53452ef22676
Mike Pierce <unrealwarrior97@gmail.com>
2024-01-14 22:24:14 +0100

    basic cpu to play with

--- projects/py-tac-toe/cpu.py
+++ projects/py-tac-toe/cpu.py
@@ -2,55 +2,125 @@ from utils import Utils
 import itertools
 edge_blocks = [[0, 0], [0, 2], [2, 0], [2, 2]]
 
+def flip_list(arr):
+    ass = []
+    for idx in range(len(arr)):
+        x = []
+        for index in range(len(arr[0])):
+            x.append(arr[index][idx])
+        ass.append(x)
+    print(ass)
+    return ass
+
 class BotPlayer(Utils):
     def __init__(self, grid) -> None:
-        self.grid = grid
+        # self.grid = grid
+        pass    
     
-    def _add_block(self, grid, pos):
-        grid[pos[0]][pos[1]] = "O"
-        return grid
+    def check_surroundings(self, grid, pos):
+        block = grid[pos[0]][pos[1]] 
+        if block == "O" or block == "B":
+            return False
+        elif block == "_":
+            return True
     
     def check_block_surroundings(self, pos_x, pos_y, grid):
+        # split a one dimensional array to chunks
         g = self.split_list(grid=grid)
-        # last played position
-        # pos_x = pos_x - 1
-        # pos_y = pos_y - 1
-        # g = self._add_block(g, pos=(pos_x, pos_y))
-
+        direction_flag = ""
         # check if there's items on the left
-        if (pos_y - 1) < 0:
-            print(f"No more blocks on the left of [{pos_x}][{pos_y}]")
-            return False
+        if (pos_y - 1) >= 0:
+            direction_flag = "left"
         else:
-            y = pos_y - 1
-            b = g[pos_x][y]
-            if b == "O":
-                flag = self.check_block_surroundings(pos_x, y, grid=[y for x in g for y in x])  
-                if flag == False:
-                    print("time to go right.") 
+            direction_flag = "right"
+        print(f"Lookup direction: {direction_flag}")
+        y = (pos_y - 1) if direction_flag == "left" else (pos_y + 1)
+        b = g[pos_x][y]
+        while True:
+            print(f"Next block lookup: grid[{pos_x}][{y}]")
+
+            if self.check_surroundings(grid=g, pos=(pos_x, y)):
+                print(f"block at [{pos_x}][{y}] was added.")
+                g[pos_x][y] = "B"
+                return g
+            else:
+                if ((y - 1) < 0):
+                    direction_flag = "right"
+                    print("going right.")
+                    y = pos_y       # restore initial position on the y-axis
+                    continue
                 else:
-                    g = flag
-                
+                    print("More blocks available. Looping...")
+                    y = y - 1 if direction_flag == "left" else y + 1
+                    if (y + 1) > len(g[0]):
+                        print("No more lookups.")
+                        return g
+                    continue
+
+        # else:
+        #     print(f"No more blocks on the left of [{pos_x}][{pos_y}]")
+        #     # check if there's blocks on the right 
+        #     if (pos_y + 1) > (len(g[0]) - 1):
+        #         print("no more blocks on the right.")
+        #     else:
+        #         y = pos_y + 1
+        #         b = g[pos_x][y]
+        #         if b == "O":
+        #             flag = self.check_block_surroundings(pos_x, y, grid=[y for x in g for y in x])
+        #         elif b == "_":                    
+        #             print(f"block at [{pos_x}][{y}] was added.")
+        #             g[pos_x][y] = "B"
+        #             return g
+                        
+                            
+
                     
-            elif b == "_":                    
-                print(f"block at [{pos_x}][{y}] was added.")
-                g[pos_x][y] = "B"
-                return g
-        if (pos_y + 1) > (len(g[0]) - 1):
-            print("no more blocks on the right.")
-        else:
-            y = pos_y + 1
-            b = g[pos_x][y]
-            if b == "O":
-                flag = self.check_block_surroundings(pos_x, y, grid=[y for x in g for y in x])
-                if flag == False:
-                    print("time to go somewhere.") 
-                else:
-                    g = flag
-            elif b == "_":                    
-                print(f"block at [{pos_x}][{y}] was added.")
-                g[pos_x][y] = "B"
-                return g
+        # if axis == "y":
+        #     # try the y-axis now
+        #     g = flip_list(g)
+        #     if (pos_x - 1) < 0:
+        #         print("no more blocks above.")
+        #         return flip_list(g)
+        #     else:
+        #         x = pos_x -1 
+                
+        #         b = g[x][pos_y]
+        #         if b == "O":
+        #             g = flip_list(g)
+        #             flag = self.check_block_surroundings(x, pos_y, grid=[y for x in g for y in x], axis="y")
+        #             if flag == False:
+        #                 print("time to go somewhere.") 
+        #             else:
+        #                 g = flag
+        #         elif b == "_":                    
+        #             print(f"block at [{x}][{pos_y}] was added.")
+        #             g[x][pos_y] = "B"
+        #             g = flip_list(g)
+        #             print("test")
+        #             return g
+                
+        #     if (pos_x + 1) > (len(g[0]) - 1):
+        #         print("no more blocks below.")
+        #         return flip_list(g)
+        #     else:
+        #         x = pos_x + 1
+                
+        #         b = g[x][pos_y]
+        #         if b == "O":
+        #             g = flip_list(g)
+        #             flag = self.check_block_surroundings(x, pos_y, grid=[y for x in g for y in x])
+        #             if flag == False:
+        #                 print("time to go somewhere.") 
+        #             else:
+        #                 g = flag
+        #         elif b == "_":                    
+        #             print(f"block at [{x}][{pos_y}] was added.")
+        #             g[x][pos_y] = "B"
+        #             g = flip_list(g)
+        #             print("test")
+        #             return g
+
+
 
 
         return g
@@ -112,11 +182,13 @@ class BotPlayer(Utils):
 
 x = BotPlayer(["_","_","_","_","_","_","_","_","_",])
 # x.bot_play()
-z = x.check_block_surroundings(pos_x=0, pos_y=2, grid=["_","O","_","_","_","_","_","_","_",])
-[print(x) for x in z]
 
-
-# g = ["_","_","_","_","_","_","_","_","_",]
-# g = x.split_list(g)
-# # z = [y for x in g for y in x]
-# print(z)
+grid = ["O","_","_","_","_","_","_","_","_",]
+while True:
+    coords = input("> enter your coordinates: ")
+    posx, posy = coords.split(" ")
+    pos = (int(posx) * 3) + int(posy)
+    grid[pos] = 'O'
+    grid = x.check_block_surroundings(pos_x=int(posx), pos_y=int(posy), grid=[grid for x in grid for grid in x])
+    print([print(g) for g in grid])
+    grid = [grid for x in grid for grid in x]
